7daedb0a1a0fac7287264a114d81b45f72fa4095
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-25 17:48:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
   
   def match(array) 
-    new_array = array.split("") 
+    new_array = array.to_s.split("") 
     word_array = @word.split("")
     new_array.sort == word_array.sort
   end
