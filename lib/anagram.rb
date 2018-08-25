57181835cfac5da3e03b1dc51fe0bcc3c564c114
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-25 17:59:45 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,19 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
+   @@match = []
   
   def initialize(word)
     @word = word
   end
   
   def match(array) 
-    new_array = array.to_s.split("").sort == word_array = @word.split("").sort
+   
+    if array.to_s.split("").sort == @word.split("").sort
+      << @word
+      @@match
+    else
+      @@match
   end
   
 end 
